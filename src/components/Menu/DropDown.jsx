765d9b23894b09fe3ff20081036c2decf60bc6aa
shvan <shvanabdulhakeem2020@gmail.com>
2024-03-21 15:29:32 +0300

    feat: 🎸 drop down icon
    Added a new drop down icon

--- src/components/Menu/DropDown.jsx
+++ src/components/Menu/DropDown.jsx
@@ -1,5 +1,7 @@
 "use client"
 import React, { useState } from 'react';
+import Image from 'next/image';
+import dropDownIcon from '/public/drop-down.svg';
 
 export default function DropDown({ menuType, selectionMenu }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,15 +15,23 @@ export default function DropDown({ menuType, selectionMenu }) {
   };
 
   return (
-    <div className="relative" onMouseEnter={() => setIsOpen(true)} onMouseLeave={() => setIsOpen(false)}>
-      <button  onClick={toggleMenu}>
+    <div className="pr-4 relative hover:bg-slate-200 " onMouseEnter={() => setIsOpen(true)} onMouseLeave={() => setIsOpen(false)}>
+      <button className='p-2 pt-1 pr-1 pb-1 flex flex-row items-center' onClick={toggleMenu}>
         {menuType}
+        <span className="pl-3">
+          <Image
+            className=''
+            src={dropDownIcon}
+            alt='search icon'
+            width={15}
+          />
+        </span>
       </button>
       {isOpen && (
-        <div className='absolute cursor-pointer'>
+        <div className='absolute cursor-pointer bg-slate-200 p-2'>
           <ul>
             {selectionMenu.map(option => (
-              <li key={option} onClick={handleOptionClick}>{option}</li>
+              <li className={`hover:text-blue-700 w-max ${ menuType === "Genres" ? "mr-7" : "mr-0" } `} key={option} onClick={handleOptionClick}>{option}</li>
             ))}
           </ul>
         </div>
