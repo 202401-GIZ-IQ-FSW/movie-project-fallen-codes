8060aca217b48668fac149d537e5572c3a4b8884
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-03-23 00:38:36 +0300

    MovieCards Component

--- src/app/actors/page.jsx
+++ src/app/actors/page.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react"
 
 export default function page() {
   return (
-    <div className='m-4 flex justify-center'>
-      <h1 className='font-bold text-2xl'>Actors</h1>
+    <div className="m-4 flex justify-center">
+      <h1 className="font-bold text-2xl">Actors</h1>
     </div>
   )
 }
