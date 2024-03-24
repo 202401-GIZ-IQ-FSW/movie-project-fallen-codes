dc9c423231748e15cef9946cd0c2438ce3349d72
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-03-24 22:08:48 +0300

    MovieCards more Design Update

--- src/components/MovieCards/MovieCards.jsx
+++ src/components/MovieCards/MovieCards.jsx
@@ -3,20 +3,11 @@ import React from "react"
 const MovieCards = ({ title, releaseYear, genre, poster }) => {
   return (
     <div
-      style={{ borderColor: "rgb(118, 171, 174)g" }}
-      className="flex flex-col bg-white shadow-md rounded-lg overflow-hidden my-4 w-56 sm:w-36 h-auto mx-2 border-solid border-2"
+      style={{ borderColor: "rgb(118, 171, 174)" }}
+      className="flex flex-col bg-white shadow-md rounded-lg overflow-hidden my-4 w-64 sm:w-52 h-auto mx-2 border-solid border-2"
     >
       <div>
         <img src={poster} alt={title} className="w-80% h-auto" />
-      </div>
-      <div className="p-4 flex flex-col justify-between leading-normal">
-        <div className="mb-8">
-          <div className="text-gray-900 font-bold text-l mb-2 font-mono">
-            {title}
-          </div>
-          <p className="text-gray-700 text-base">{releaseYear}</p>
-          <p className="text-gray-700 text-base">{genre}</p>
-        </div>
       </div>
     </div>
   )
