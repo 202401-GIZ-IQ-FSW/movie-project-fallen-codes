8977480aa3a17fdda0243cc84234a4ff6e84130c
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-03-24 21:50:56 +0300

    Design Update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,80 +1,120 @@
-import React from 'react'
+import React from "react"
 export default function Footer() {
   return (
-    <div className='m-4 flex justify-center rounded-lg bg-red-900'>
-      <div className='container m-4 rounded-lg '>
-       
-      <h1 className='font-mono font-bold text-gray-400 text-2xl flex justify-center'>Fallen Codes</h1>
-     
+    <div
+      style={{ backgroundColor: "rgb(49, 54, 63)" }}
+      className="m-4 flex justify-center rounded-lg"
+    >
+      <div className="container m-4 rounded-lg ">
+        <h1 className="font-mono font-bold text-gray-400 text-2xl flex justify-center">
+          Fallen Codes
+        </h1>
 
-<div class="container mx-auto py-8">
-    <div class="flex flex-wrap -mx-4">
-        <div class="flex flex-col flex-grow justify-between items-center md:flex-row md:w-1/4 px-4 mb-4">
-            <div class="bg-black rounded-lg shadow p-6 w-full">
-                <h3 class="text-lg font-semibold mb-2 text-gray-400 flex justify-center">Aween</h3>
+        <div class="container mx-auto py-8">
+          <div class="flex flex-wrap -mx-4">
+            <div class="flex flex-col flex-grow justify-between items-center md:flex-row md:w-1/4 px-4 mb-4">
+              <div class="bg-black rounded-lg shadow p-6 w-full">
+                <h3 class="text-lg font-semibold mb-2 text-gray-400 flex justify-center">
+                  Aween
+                </h3>
                 <div class="flex justify-center space-x-4">
-                    <a href="https://github.com/" target="_blank" rel="noopener noreferrer" class="text-red-900 hover:underline flex items-center">
-                        <i class="fab fa-github mr-2"></i>
-                        
-                    </a>
-                    <a href="https://www.linkedin.com/in/aween-ezzat-/" target="_blank" rel="noopener noreferrer" class="text-red-900 hover:underline flex items-center">
-                        <i class="fab fa-linkedin mr-2"></i>
-                       
-                    </a>
+                  <a
+                    href="https://github.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="text-red-900 hover:underline flex items-center"
+                  >
+                    <i class="fab fa-github mr-2"></i>
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/aween-ezzat-/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="text-red-900 hover:underline flex items-center"
+                  >
+                    <i class="fab fa-linkedin mr-2"></i>
+                  </a>
                 </div>
+              </div>
             </div>
-        </div>
-        <div class="flex flex-col flex-grow justify-between items-center md:flex-row md:w-1/4 px-4 mb-4">
-            <div class="bg-black rounded-lg shadow p-6 w-full">
-                <h3 class="text-lg font-semibold mb-2 text-gray-400 flex justify-center">Shvan</h3>
+            <div class="flex flex-col flex-grow justify-between items-center md:flex-row md:w-1/4 px-4 mb-4">
+              <div class="bg-black rounded-lg shadow p-6 w-full">
+                <h3 class="text-lg font-semibold mb-2 text-gray-400 flex justify-center">
+                  Shvan
+                </h3>
                 <div class="flex justify-center space-x-4">
-                    <a href="https://github.com/Sh89hkm" target="_blank" rel="noopener noreferrer" class="text-red-900 hover:underline flex items-center">
-                        <i class="fab fa-github mr-2"></i>
-                        
-                    </a>
-                    <a href="https://www.linkedin.com/in/sh89hkm/" target="_blank" rel="noopener noreferrer" class="text-red-900 hover:underline flex items-center">
-                        <i class="fab fa-linkedin mr-2"></i>
-                       
-                    </a>
+                  <a
+                    href="https://github.com/Sh89hkm"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="text-red-900 hover:underline flex items-center"
+                  >
+                    <i class="fab fa-github mr-2"></i>
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/sh89hkm/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="text-red-900 hover:underline flex items-center"
+                  >
+                    <i class="fab fa-linkedin mr-2"></i>
+                  </a>
                 </div>
+              </div>
             </div>
-        </div>
-        <div class="flex flex-col flex-grow justify-between items-center md:flex-row md:w-1/4 px-4 mb-4">
-            <div class="bg-black rounded-lg shadow p-6 w-full">
-                <h3 class="text-lg font-semibold mb-2 text-gray-400 flex justify-center">Dilan</h3>
+            <div class="flex flex-col flex-grow justify-between items-center md:flex-row md:w-1/4 px-4 mb-4">
+              <div class="bg-black rounded-lg shadow p-6 w-full">
+                <h3 class="text-lg font-semibold mb-2 text-gray-400 flex justify-center">
+                  Dilan
+                </h3>
                 <div class="flex justify-center space-x-4">
-                    <a href="https://www.linkedin.com/in/dilan-ahmed-02b671212/" target="_blank" rel="noopener noreferrer" class="text-red-900 hover:underline flex items-center">
-                        <i class="fab fa-github mr-2"></i>
-                        
-                    </a>
-                    <a href="https://github.com/Dilan-Ahmed" target="_blank" rel="noopener noreferrer" class="text-red-900 hover:underline flex items-center">
-                        <i class="fab fa-linkedin mr-2"></i>
-                        
-                    </a>
+                  <a
+                    href="https://www.linkedin.com/in/dilan-ahmed-02b671212/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="text-red-900 hover:underline flex items-center"
+                  >
+                    <i class="fab fa-github mr-2"></i>
+                  </a>
+                  <a
+                    href="https://github.com/Dilan-Ahmed"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="text-red-900 hover:underline flex items-center"
+                  >
+                    <i class="fab fa-linkedin mr-2"></i>
+                  </a>
                 </div>
+              </div>
             </div>
-        </div>
-        <div class="flex flex-col flex-grow justify-between items-center md:flex-row md:w-1/4 px-4 mb-4">
-            <div class="bg-black rounded-lg shadow p-6 w-full">
-                <h3 class="text-lg font-semibold mb-2 text-gray-400 flex justify-center">Astevan</h3>
+            <div class="flex flex-col flex-grow justify-between items-center md:flex-row md:w-1/4 px-4 mb-4">
+              <div class="bg-black rounded-lg shadow p-6 w-full">
+                <h3 class="text-lg font-semibold mb-2 text-gray-400 flex justify-center">
+                  Astevan
+                </h3>
                 <div class="flex justify-center space-x-4">
-                    <a href="https://github.com/person4" target="_blank" rel="noopener noreferrer" class="text-red-900 hover:underline flex items-center">
-                        <i class="fab fa-github mr-2"></i>
-                        
-                    </a>
-                    <a href="https://linkedin.com/in/person4" target="_blank" rel="noopener noreferrer" class="text-red-900 hover:underline flex items-center">
-                        <i class="fab fa-linkedin mr-2"></i>
-                        
-                    </a>
+                  <a
+                    href="https://github.com/person4"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="text-red-900 hover:underline flex items-center"
+                  >
+                    <i class="fab fa-github mr-2"></i>
+                  </a>
+                  <a
+                    href="https://linkedin.com/in/person4"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="text-red-900 hover:underline flex items-center"
+                  >
+                    <i class="fab fa-linkedin mr-2"></i>
+                  </a>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-</div>
-
-
-
-    </div>
+      </div>
     </div>
   )
 }
