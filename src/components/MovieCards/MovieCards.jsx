223de424dbc34def96e2daa723f0bb0de9c3551a
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-03-24 22:23:12 +0300

    GeneralUpdate for Design

--- src/components/MovieCards/MovieCards.jsx
+++ src/components/MovieCards/MovieCards.jsx
@@ -4,7 +4,7 @@ const MovieCards = ({ title, releaseYear, genre, poster }) => {
   return (
     <div
       style={{ borderColor: "rgb(118, 171, 174)" }}
-      className="flex flex-col bg-white shadow-md rounded-lg overflow-hidden my-4 w-64 sm:w-52 h-auto mx-2 border-solid border-2"
+      className="flex flex-col bg-white shadow-md rounded-lg overflow-hidden my-4 w-64 sm:w-52 h-auto mx-2 border-solid border-2 "
     >
       <div>
         <img src={poster} alt={title} className="w-80% h-auto" />
