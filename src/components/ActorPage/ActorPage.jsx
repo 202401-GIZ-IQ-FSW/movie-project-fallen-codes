8c7f4158fcb8e3a2b2c93bcf574fdba7577bf0d5
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-03-24 18:09:03 +0300

    ActorPage Design Updated

--- src/components/ActorPage/ActorPage.jsx
+++ src/components/ActorPage/ActorPage.jsx
@@ -2,15 +2,42 @@ import React from "react"
 
 const ActorPage = () => {
   return (
-    <div className="flex-row gap-50 ">
-      <div className="w-350px mx-auto my-0">
-        <img alt={title} src="URL" className="rounded-md w-full" />
+    <div className="mx-auto p-16 grid-cols-2 w-full flex justify-between border-solid border-2 border-red-800">
+      <div className="w-80 md:w-80 border-solid border-2 border-red-800">
+        <img
+          src={actorpicture}
+          alt={title}
+          className=" mx-auto rounded-lg shadow-lg"
+        />
       </div>
-      <div className="w-50%">
-        <h2> {actorname}</h2>
-        <h4> {gender}</h4>
-        <h5> {birthday}</h5>
-        <p className="w-50% text-gray-800">{biography}</p>
+      <div>
+        <h1 className="text-3xl  px-4 py-2 font-bold mb-2 bg-slate-200 rounded-md w-56">
+          {actorName}
+        </h1>
+        <p>
+          <span className="font-bold">Birth Date:</span> {birthdate}
+        </p>
+        <p>
+          <span className="font-bold">Gender:</span>
+          {gender}
+        </p>
+        <p>
+          <span className="font-bold">From:</span> {country}
+        </p>
+
+        <p className="py-3 my-2">{biography}</p>
+
+        <div className="mt-4">
+          <h2 className="text-xl font-bold mb-2">Famous Movies</h2>
+          <ul>
+            <li>{movie}</li>
+            <li>{movie}</li>
+            <li>{movie}</li>
+            <li>{movie}</li>
+            <li>{movie}</li>
+            <li>{movie}</li>
+          </ul>
+        </div>
       </div>
     </div>
   )
