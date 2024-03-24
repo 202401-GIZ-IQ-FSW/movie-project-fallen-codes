faa14671290ec515dfeed711b77a0cfbfe954ca0
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-03-24 15:05:18 +0300

    Movie Cards and Actor Cards Updated

--- src/components/ActorCards/ActorCards.jsx
+++ src/components/ActorCards/ActorCards.jsx
@@ -2,10 +2,14 @@ import React from "react"
 
 const ActorCards = ({ name, image }) => {
   return (
-    <div className="actor-card bg-yellow-50 flex flex-col lg:flex-row lg:flex-wrap shadow-md rounded-lg overflow-hidden my-4 w-100% max-w-150 ">
-      <img srcs={imageURL} alt={actor - pic} className="w-full" />
+    <div className="flex flex-col bg-white shadow-md rounded-lg overflow-hidden my-4 w-64 sm:w-44 h-auto mx-2 border-solid border-gray-600 border-2 ">
       <div>
-        <h2 className=" text-bold text-gray-600 text-lg ">{name}</h2>
+        <img srcs={imageURL} alt={actor - pic} className="w-80% h-auto" />
+      </div>
+      <div>
+        <h2 className=" text-bold text-center text-gray-600 text-lg  ">
+          {name}
+        </h2>
       </div>
     </div>
   )
--- src/components/MovieCards/MovieCards.jsx
+++ src/components/MovieCards/MovieCards.jsx
@@ -2,13 +2,15 @@ import React from "react"
 
 const MovieCards = ({ title, releaseYear, genre, poster }) => {
   return (
-    <div className="flex flex-col lg:flex-row lg:flex-wrap bg-white shadow-md rounded-lg overflow-hidden my-4 w-100% max-w-150 ">
-      <img src={poster} alt={title} className="w-100% md:w-50%" />
+    <div className="flex flex-col bg-white shadow-md rounded-lg overflow-hidden my-4 w-56 sm:w-36 h-auto mx-2 border-solid border-gray-600 border-2 ">
+      <div>
+        <img src={poster} alt={title} className="w-80% h-auto" />
+      </div>
       <div className="p-4 flex flex-col justify-between leading-normal">
         <div className="mb-8">
           <div className="text-gray-900 font-bold text-l mb-2">{title}</div>
-          <p className="text-gray-700 text-base">Release Year: {releaseYear}</p>
-          <p className="text-gray-700 text-base">Genre: {genre}</p>
+          <p className="text-gray-700 text-base">{releaseYear}</p>
+          <p className="text-gray-700 text-base">{genre}</p>
         </div>
       </div>
     </div>
