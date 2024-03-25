bd42cddada516f7c0023bcf844cee1ecfd6bc026
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-03-26 00:17:32 +0300

    Major Fix in Responsivenees

--- src/components/Pages/MoviePage.jsx
+++ src/components/Pages/MoviePage.jsx
@@ -13,13 +13,18 @@ const MoviePage = ({ movie }) => {
   return (
     <>
       {movie && (
-        <main style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        
-         }}>
-          <div style={{backdropFilter: 'blur(3px)'}} className="mx-auto p-16 grid-cols-2 w-full flex justify-between sd:flex-wrap">
+        <main
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
+        >
+          <div
+            style={{ backdropFilter: "blur(3px)" }}
+            className="mx-auto p-16 grid-cols-2 w-full  md:flex justify-between "
+          >
             <div className="mr-4 md:w-80 lg:w-96 rounded-lg">
               <img
                 src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
@@ -27,7 +32,10 @@ const MoviePage = ({ movie }) => {
                 className="w-auto h-auto mx-auto rounded-lg shadow-lg"
               />
             </div>
-            <div style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)' }} className="p-4 rounded-lg lg: max-w-8/12 md:w-2/3">
+            <div
+              style={{ backgroundColor: "rgba(255, 255, 255, 0.8)" }}
+              className="p-4 rounded-lg lg: max-w-8/12 md:w-2/3"
+            >
               <h1
                 style={{ color: "rgb(34, 40, 49)" }}
                 className="text-3xl  px-4 py-2 font-bold mb-2 bg-slate-200 rounded-md w-64 shadow-md hover:bg-slate-400"
@@ -41,7 +49,7 @@ const MoviePage = ({ movie }) => {
                 >
                   Release Date:{" "}
                 </span>
-                <span style={{ color: "rgb(118, 171, 174)" }}>
+                <span style={{ color: "rgb(252, 103, 54)" }}>
                   {movie.release_date}
                 </span>
               </p>
@@ -52,7 +60,7 @@ const MoviePage = ({ movie }) => {
                 >
                   Runtime:{" "}
                 </span>
-                <span style={{ color: "rgb(118, 171, 174)" }}>
+                <span style={{ color: "rgb(252, 103, 54)" }}>
                   {movie.runtime}
                 </span>
               </p>
@@ -63,18 +71,18 @@ const MoviePage = ({ movie }) => {
                 >
                   Language:{" "}
                 </span>{" "}
-                <span style={{ color: "rgb(118, 171, 174)" }}>
+                <span style={{ color: "rgb(252, 103, 54)" }}>
                   {movie.original_language}
                 </span>
               </p>
               <p>
                 <span
-                  style={{ color: "rgb(49, 54, 63)" }}
+                  style={{ color: "rgb(252, 103, 54)" }}
                   className="font-bold"
                 >
                   Rating:{" "}
                 </span>
-                <span style={{ color: "rgb(118, 171, 174)" }}>{ratings}</span>
+                <span style={{ color: "rgb(252, 103, 54)" }}>{ratings}</span>
               </p>
               <p>
                 <span
@@ -83,7 +91,7 @@ const MoviePage = ({ movie }) => {
                 >
                   Votes:{" "}
                 </span>
-                <span style={{ color: "rgb(118, 171, 174)" }}>
+                <span style={{ color: "rgb(252, 103, 54)" }}>
                   {movie.vote_count}
                 </span>
               </p>
