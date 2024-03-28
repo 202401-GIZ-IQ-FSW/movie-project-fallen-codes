3f30ba99101dcde916de62f999c201d5e2bbd29d
shvan <shvanabdulhakeem2020@gmail.com>
2024-03-28 23:24:56 +0300

    minor fix

--- src/data/moviesData.js
+++ src/data/moviesData.js
@@ -1,16 +1,16 @@
-const API_KEY = "87227e85299796fb4f134e6af5a95925" ;
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 const baseUrl = "https://api.themoviedb.org/3/movie/";
-const apiKey = `?api_key=${API_KEY}&language=en-US&page=`; 
+const urlApiKey = `?api_key=${apiKey}&language=en-US&page=`; 
 
 export const moviesData = async (pageId=1, urlCategory="now_playing") => {
-    const url = baseUrl + urlCategory + apiKey + pageId.toString();
+    const url = baseUrl + urlCategory + urlApiKey + pageId.toString();
     const res = await fetch(url);
     const data = res.json();
     return data;
 };
 
 const baseUrlAll = "https://api.themoviedb.org/3/discover/movie"
-const apiKeyAll = `?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=`;
+const apiKeyAll = `?api_key=${apiKey}&include_adult=false&include_video=false&language=en-US&page=`;
 const sort = "&sort_by=popularity.desc";
 
 export const moviesDataAll = async (pageId=1) => {
