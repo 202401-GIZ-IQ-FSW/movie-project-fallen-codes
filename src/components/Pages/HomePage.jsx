343908da1f7251158175b02f70d47b9fa3573fe3
shvan <shvanabdulhakeem2020@gmail.com>
2024-04-07 19:47:15 +0300

    new update

--- src/components/Pages/HomePage.jsx
+++ src/components/Pages/HomePage.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import Link from "next/link"
 import MovieCard from "@/components/Cards/MovieCard"
 import Navbar from "@/components/Navbar/Navbar"
